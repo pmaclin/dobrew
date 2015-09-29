2bc5db132a8b30efaca6afd6f864bc8501295dcd
pmaclin <pcharlesantonio@gmail.com>
2015-09-29 16:30:09 -0500

    Edits Config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/pmaclin/dobrew.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, :master
+# set :branch, :master
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
