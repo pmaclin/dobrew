f97bb7080b1f26048d9d2745f91927d50fb8d568
Maclin Phil <philmaclin@gmail.com>
2015-12-02 11:46:06 -0600

    Enables Active Admin user model
    1. In order to be able to edit via Active Admin interface
    2. Further branding error page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,8 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :std_flavor_profile, :com_ex_image, :created_at, :updated_at
+
 #
 # or
 #
