c9ad1be85c29de62d651af33c172189a0d0e8ff4
Maclin Phil <philmaclin@gmail.com>
2015-12-02 22:10:57 -0600

    Adjusts style.rb in Active Admin (App/admin/style.rb)
    This works on the desktop. Pushing to production.
    
    permit_params :name, :std_flavor_profile, :com_ex_image

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :std_flavor_profile, :com_ex_image
+# permit_params :name, :std_flavor_profile, :com_ex_image
 
 #
 # or
