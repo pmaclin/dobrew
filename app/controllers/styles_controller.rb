8abcf7414b71b6a1796b83989de120601863bde1
Maclin Phil <philmaclin@gmail.com>
2015-12-02 22:22:28 -0600

    Comments out destroy action in styles controller
    Able to edit a style through Active Admin, but cannot destroy the style
    (duplicates in production).
    Commented out the code to mimic what the edit action looks like.
    Pushing to prod and testing.

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -56,11 +56,11 @@ class StylesController < ApplicationController
   # DELETE /styles/1
   # DELETE /styles/1.json
   def destroy
-    @style.destroy
-    respond_to do |format|
-      format.html { redirect_to styles_url, notice: 'Style was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # @style.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to styles_url, notice: 'Style was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
