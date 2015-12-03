8ce05961a0da0794a758a3a890e8745bf22071df
Maclin Phil <philmaclin@gmail.com>
2015-12-03 11:54:15 -0600

    Adds confirmation mail functionality
    development.rb
    production.rb
    Edits Devise Users Migration, uncommenting Confirmable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = {:host => 'gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port    => "587", #verify this port!!
+    :domain  => "gmail.com"
+    :authentication => :login,
+    :user_name => "[username]",
+    :password => "[password]"
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
