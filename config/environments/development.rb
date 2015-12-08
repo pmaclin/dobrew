96f1f2ef0aef78dff5d866432b90d809a216a950
Maclin Phil <philmaclin@gmail.com>
2015-12-07 21:47:03 -0600

    Removes Confirmable
    Emails are not going out, so confirmable installed keeps users from
    completing their registrations.
    Back to the drawing board

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,11 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     # address: "smtp.gmail.com",
-    # port: 587
-    # domain: ENV["GMAIL_DOMAIN"]
-    # authentication: "plain"
-    # enable_starttls_auto: false
-    # user_name: ENV["GMAIL_USERNAME"]
+    # port: 25,
+    # domain: ENV["GMAIL_DOMAIN"],
+    # authentication: "plain",
+    # enable_starttls_auto: false,
+    # user_name: ENV["GMAIL_USERNAME"],
     # password: ENV["GMAIL_PASSWORD"]
   }
 
