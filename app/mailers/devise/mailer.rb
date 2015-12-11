c1de48338194458b0cdeff70fd106a446cb8db01
Maclin Phil <philmaclin@gmail.com>
2015-12-11 17:41:26 -0600

    Sets up test environment for mail

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -2,10 +2,10 @@ if defined?(ActionMailer)
   class Devise::Mailer < Devise.parent_mailer.constantize
     include Devise::Mailers::Helpers
 
-    def confirmation_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :confirmation_instructions, opts)
-    end
+    # def confirmation_instructions(record, token, opts={})
+    #   @token = token
+    #   devise_mail(record, :confirmation_instructions, opts)
+    # end
 
     def reset_password_instructions(record, token, opts={})
       @token = token
